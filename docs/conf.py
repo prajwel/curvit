daa4759519be3b60e258b6ec7b369f7d200e14d6
prajwel <prajwel.pj@gmail.com>
2021-06-26 02:11:56 +0530

    logo replaces name on sidebar

--- docs/conf.py
+++ docs/conf.py
@@ -53,3 +53,11 @@ html_theme = 'furo'
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+#Stuff below is scavenged from the web
+html_logo = "curvit_logo.svg"
+html_theme_options = {
+    "sidebar_hide_name": True,
+    "navigation_with_keys": True,
+}
+
