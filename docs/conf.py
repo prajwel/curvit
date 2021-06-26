7f6d5368509d89813fab5a89dc7a094c67a3d6f1
prajwel <prajwel.pj@gmail.com>
2021-06-26 18:06:29 +0530

    docs bug fixes

--- docs/conf.py
+++ docs/conf.py
@@ -32,7 +32,8 @@ release = ''
 # ones.
 extensions = ['sphinx_copybutton',
               'sphinx.ext.napoleon',
-              'sphinx.ext.autodoc'
+              'sphinx.ext.autodoc',
+              'sphinx.ext.autosectionlabel'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
