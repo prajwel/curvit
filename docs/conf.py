f30092d13c38aea023c43255ceb3acdf8d200197
prajwel <prajwel.pj@gmail.com>
2021-06-26 10:45:18 +0530

    copy button added

--- docs/conf.py
+++ docs/conf.py
@@ -30,7 +30,7 @@ release = '1.2.5'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
+extensions = ['sphinx_copybutton'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
